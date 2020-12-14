13f84edba98129f648ad259e2b27e038c1cc6c08
Louisemehouas <45461620+Louisemehouas@users.noreply.github.com>
2020-12-14 10:25:02 +0100

    Create tests

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,13 @@
+const assert = require ('assert`);
+
+
+
+assert.strictEqual(typeof capitalizeFirst,'function');
+
+assert.strictEqual(capitalizeFirst('a') 'A');
+
+assert.strictequal(capitalizeFirst('') '');
+
+assert.strictEqual(capitalizeFirst('i am learning javascript') 'I Am Learning Javascript');
+
+assert.strictEqual(capitalizeFirst.length, 1);
